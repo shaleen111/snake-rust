1c177442f391cd95e3ca8a22b0fe2f008a90e239
Shaleen Baral <shaleenbaral111@gmail.com>
2021-06-18 21:52:10 +0545

    Updated draw module to ggez

--- snake/src/draw.rs
+++ snake/src/draw.rs
@@ -1,11 +1,11 @@
-use piston_window::{rectangle, G2d, Context};
-use piston_window::types::Color;
+use ggez::{Context, graphics, graphics::Color, graphics::Mesh,
+           graphics::DrawMode, graphics::DrawParam};
 
-const SCALE_FACTOR: f64 = 25.0;
+const SCALE_FACTOR: f32 = 25.0;
 
-pub fn to_coord(game_coord: i32) -> f64
+pub fn to_coord(game_coord: i32) -> f32
 {
-    (game_coord as f64) * SCALE_FACTOR
+    (game_coord as f32) * SCALE_FACTOR
 }
 
 pub fn draw_rect( game_x: i32,
@@ -13,21 +13,18 @@ pub fn draw_rect( game_x: i32,
                   width: i32,
                   height: i32,
                   color: Color,
-                  con: &Context,
-                  g: &mut G2d)
+                  ctx: &mut Context)
 {
-    let actual_x: f64 = to_coord(game_x);
-    let actual_y: f64 = to_coord(game_y);
+    let actual_x: f32 = to_coord(game_x);
+    let actual_y: f32 = to_coord(game_y);
 
-    rectangle(
-        color,
-        [actual_x, actual_y, SCALE_FACTOR * (width as f64), SCALE_FACTOR * (height as f64)],
-        con.transform,
-        g
-    );
+    let r = graphics::Rect::new(actual_x, actual_y, to_coord(width), to_coord(height));
+    let mesh_r = Mesh::new_rectangle(ctx, DrawMode::fill(), r, color).expect("Error making rectangle mesh.");
+
+    graphics::draw(ctx, &mesh_r, DrawParam::default()).expect("Error trying to draw rectangle mesh.");
 }
 
-pub fn draw_block(game_x: i32, game_y: i32, color: Color, con: &Context, g: &mut G2d)
+pub fn draw_block(game_x: i32, game_y: i32, color: Color, ctx: &mut Context)
 {
-    draw_rect(game_x, game_y, 1, 1, color, con, g);
+    draw_rect(game_x, game_y, 1, 1, color, ctx);
 }
--- snake/src/main.rs
+++ snake/src/main.rs
@@ -8,7 +8,6 @@
 // use crate::game::Game;
 // use crate::draw::to_coord;
 
-// const BG_COLOR: Color = [0.5, 0.5, 0.5, 1.0];
 
 // fn main()
 // {
@@ -23,48 +22,64 @@
 
     // while let Some(event) = window.next()
     // {
-    //     if let Some(Button::Keyboard(key)) = event.press_args()
-    //     {
-    //         game.key_pressed(key);
-    //     }
+        //     if let Some(Button::Keyboard(key)) = event.press_args()
+        //     {
+            //         game.key_pressed(key);
+            //     }
 
-    //     window.draw_2d(&event, |c, g, _| {
-    //         clear(BG_COLOR, g);
-    //         game.draw(&c, g);
-    //     });
+            //     window.draw_2d(&event, |c, g, _| {
+                //         clear(BG_COLOR, g);
+                //         game.draw(&c, g);
+                //     });
 
-    //     event.update(|arg| {
-    //         game.update(arg.dt);
-    //     });
-    // }
+                //     event.update(|arg| {
+                    //         game.update(arg.dt);
+                    //     });
+                    // }
 
-// }
+                    // }
 
-use ggez::{event::EventHandler};
+mod draw;
+
+use ggez::{ContextBuilder, event, conf,
+           graphics, graphics::Color};
+
+use crate::draw::{to_coord, draw_block};
+
+const BG_COLOR: Color = Color::new(0.5, 0.5, 0.5, 1.0);
 
 struct WindowState
 {
-
+    game: (),
 }
 
-impl EventHandler for WindowState
+impl event::EventHandler for WindowState
 {
-    fn update(&mut self, ctx: &mut ggez::Context) -> ggez::GameResult
+    fn update(&mut self, _ctx: &mut ggez::Context) -> ggez::GameResult
     {
         Ok(())
     }
 
     fn draw(&mut self, ctx: &mut ggez::Context) -> ggez::GameResult
     {
+        graphics::clear(ctx, BG_COLOR);
+
+        draw_block(0, 0, Color::new(1.0, 0.0, 0.0, 1.0), ctx);
+
+        graphics::present(ctx).expect("Error presenting.");
         Ok(())
     }
 }
 
 fn main()
 {
-    let (ctx, event_loop) = ggez::ContextBuilder::new("Snake", "Shaleen Baral").build().unwrap();
+    let (width, height) = (20, 20);
+    let (ctx, event_loop) = ContextBuilder::new("Snake", "Shaleen Baral")
+                            .window_mode(conf::WindowMode::default().dimensions(to_coord(width) as f32, to_coord(height) as f32))
+                            .build()
+                            .expect("Error building context.");
 
-    let window_state = WindowState{};
+    let window_state = WindowState{game: ()};
 
     ggez::event::run(ctx, event_loop, window_state);
 }
