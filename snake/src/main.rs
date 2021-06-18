5fc51f9fdea0b67c7799ab75a15eeb44cf7876af
Shaleen Baral <shaleenbaral111@gmail.com>
2021-06-18 20:38:11 +0545

    Start of move to ggez

--- snake/src/main.rs
+++ snake/src/main.rs
@@ -1,40 +1,70 @@
-mod draw;
-mod snake;
-mod game;
+// mod draw;
+// mod snake;
+// mod game;
 
-use piston_window::*;
-use piston_window::types::Color;
+// use piston_window::*;
+// use piston_window::types::Color;
 
-use crate::game::Game;
-use crate::draw::to_coord;
+// use crate::game::Game;
+// use crate::draw::to_coord;
 
-const BG_COLOR: Color = [0.5, 0.5, 0.5, 1.0];
+// const BG_COLOR: Color = [0.5, 0.5, 0.5, 1.0];
+
+// fn main()
+// {
+    // let (width, height) = (20, 20);
+
+    // let mut window: PistonWindow = WindowSettings::new("Snake",
+    //                                 [to_coord(width), to_coord(height)])
+    //                                 .exit_on_esc(true)
+    //                                 .build()
+    //                                 .unwrap();
+    // let mut game: Game = Game::new(width, height);
+
+    // while let Some(event) = window.next()
+    // {
+    //     if let Some(Button::Keyboard(key)) = event.press_args()
+    //     {
+    //         game.key_pressed(key);
+    //     }
+
+    //     window.draw_2d(&event, |c, g, _| {
+    //         clear(BG_COLOR, g);
+    //         game.draw(&c, g);
+    //     });
+
+    //     event.update(|arg| {
+    //         game.update(arg.dt);
+    //     });
+    // }
+
+// }
+
+use ggez::{event::EventHandler};
+
+struct WindowState
+{
+
+}
+
+impl EventHandler for WindowState
+{
+    fn update(&mut self, ctx: &mut ggez::Context) -> ggez::GameResult
+    {
+        Ok(())
+    }
+
+    fn draw(&mut self, ctx: &mut ggez::Context) -> ggez::GameResult
+    {
+        Ok(())
+    }
+}
 
 fn main()
 {
-    let (width, height) = (20, 20);
+    let (ctx, event_loop) = ggez::ContextBuilder::new("Snake", "Shaleen Baral").build().unwrap();
 
-    let mut window: PistonWindow = WindowSettings::new("Snake",
-                                    [to_coord(width), to_coord(height)])
-                                    .exit_on_esc(true)
-                                    .build()
-                                    .unwrap();
-    let mut game: Game = Game::new(width, height);
+    let window_state = WindowState{};
 
-    while let Some(event) = window.next()
-    {
-        if let Some(Button::Keyboard(key)) = event.press_args()
-        {
-            game.key_pressed(key);
-        }
-
-        window.draw_2d(&event, |c, g, _| {
-            clear(BG_COLOR, g);
-            game.draw(&c, g);
-        });
-
-        event.update(|arg| {
-            game.update(arg.dt);
-        });
-    }
+    ggez::event::run(ctx, event_loop, window_state);
 }
